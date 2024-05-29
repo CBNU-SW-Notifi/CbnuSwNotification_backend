b138b9c52b1c884e6083f94eee7b74760fe10ab5
LEE026 <lhs456852@gmail.com>
2024-05-29 16:15:40 +0900

    spring kotlin으로 초기 세팅

---
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/CbnuSwNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.CbnuSwNotification.CbnuSwNotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CbnuSwNotificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<CbnuSwNotificationApplication>(*args)
+}

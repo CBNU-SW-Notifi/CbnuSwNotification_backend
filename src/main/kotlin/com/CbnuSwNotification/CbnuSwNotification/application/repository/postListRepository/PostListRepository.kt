7eecc89cf373caffa96eb86d6cab3ceeb900d3ec
LEE026 <lhs456852@gmail.com>
2024-06-01 15:30:37 +0900

    페이지 목록을 조회할 레파지토리 인터페이스

---
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/repository/postListRepository/PostListRepository.kt
@@ -0,0 +1,11 @@
+package com.CbnuSwNotification.CbnuSwNotification.application.repository.postListRepository
+
+interface PostListRepository {
+
+    /**
+     * cursor: 어디 이후로 부터 조회할지
+     * size: 몇개를 반환할지
+     * return cursor 위치 이후 부터 size개
+     */
+    fun getPostList(cursor:Long,size:Long)
+}

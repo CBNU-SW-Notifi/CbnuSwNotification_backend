d7f4cc7f28cbedba3d81e574a050a33379c4774c
LEE026 <lhs456852@gmail.com>
2024-06-01 15:33:33 +0900

    주석 변경

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/repository/postListRepository/PostListRepository.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/repository/postListRepository/PostListRepository.kt
@@ -3,9 +3,9 @@ package com.CbnuSwNotification.CbnuSwNotification.application.repository.postLis
 interface PostListRepository {
 
     /**
-     * cursor: 어디 이후로 부터 조회할지
-     * size: 몇개를 반환할지
-     * return cursor 위치 이후 부터 size개
+     * @param cursor: 어디 이후로 부터 조회할지
+     * @param size: 몇개를 반환할지
+     * @return cursor 위치 이후 부터 size개
      */
     fun getPostList(cursor:Long,size:Long)
 }

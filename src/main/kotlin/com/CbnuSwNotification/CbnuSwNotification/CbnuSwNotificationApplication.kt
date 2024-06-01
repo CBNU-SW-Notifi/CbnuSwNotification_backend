09f32b83edd9e379d0f807ea1f693474bb5d2937
LEE026 <lhs456852@gmail.com>
2024-06-01 15:28:53 +0900

    시작 자동 등록을 위한 어너테이션 추가

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/CbnuSwNotificationApplication.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/CbnuSwNotificationApplication.kt
@@ -2,8 +2,10 @@ package com.CbnuSwNotification.CbnuSwNotification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class CbnuSwNotificationApplication
 
 fun main(args: Array<String>) {

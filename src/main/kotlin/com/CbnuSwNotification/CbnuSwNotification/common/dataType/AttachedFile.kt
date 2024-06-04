829653bc819fad88c53f47cff5e9a525077a7e80
LEE026 <lhs456852@gmail.com>
2024-06-04 19:21:47 +0900

    첨부파일의 자료형을 변경

---
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/common/dataType/AttachedFile.kt
@@ -0,0 +1,6 @@
+package com.CbnuSwNotification.CbnuSwNotification.common.dataType
+
+data class AttachedFile(
+    val name: String,
+    val url: String,
+)
--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/common/dto/postReadDto/PostReadResponse.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/common/dto/postReadDto/PostReadResponse.kt
@@ -1,5 +1,6 @@
 package com.CbnuSwNotification.CbnuSwNotification.common.dto.postReadDto
 
+import com.CbnuSwNotification.CbnuSwNotification.common.dataType.AttachedFile
 import com.CbnuSwNotification.CbnuSwNotification.common.dataType.PostType
 import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
@@ -9,7 +10,7 @@ data class PostReadResponse(
     val title: String,
     val content: String,
     val imageUrls: List<String>,
-    val attachedFiles: List<String>,
+    val attachedFiles: List<AttachedFile>,
     val postType: PostType,
     @JsonFormat(
         shape = JsonFormat.Shape.STRING,

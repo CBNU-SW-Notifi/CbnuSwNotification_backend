66d9512b0be9b7c32290bc2b8a4dda14a7f882f0
LEE026 <lhs456852@gmail.com>
2024-06-06 12:03:30 +0900

    AttachedFileUrl의 레파지토리 인터페이스 제작

---
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/repository/attachedFileUrlRepository/AttachedFileUrlRepository.kt
@@ -0,0 +1,26 @@
+package com.CbnuSwNotification.CbnuSwNotification.application.repository.attachedFileUrlRepository
+
+import com.CbnuSwNotification.CbnuSwNotification.application.domain.post.AttachedFileUrl
+import com.CbnuSwNotification.CbnuSwNotification.application.domain.post.Post
+
+interface AttachedFileUrlRepository {
+
+    /**
+     * @param attachedFileUrl 첨부파일 url 객체
+     * @return 저장된 객체의 id
+     */
+    fun save(attachedFileUrl: AttachedFileUrl): Long
+
+    /**
+     * @param id 찾고싶은 AttachedFileUrl의 id
+     * @return 찾고자한 AttachedFileUrl 객체
+     */
+    fun findById(id: Long): AttachedFileUrl
+
+    /**
+     * @param post 첨부파일 리스트를 얻고자하는 POST객체
+     * @return 해당 POST에 포함된 AttachedFileUrl리스트
+     */
+    fun findAllByPost(post: Post): List<AttachedFileUrl>
+
+}

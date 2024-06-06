d0cee49141b30de0701cc7e05f05f12ae2090887
LEE026 <lhs456852@gmail.com>
2024-06-06 12:13:13 +0900

    ImageUrlRepository 구현제 제작

---
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/repository/imageUrlRepository/DbImageUrlRepository.kt
@@ -0,0 +1,32 @@
+package com.CbnuSwNotification.CbnuSwNotification.application.repository.imageUrlRepository
+
+import com.CbnuSwNotification.CbnuSwNotification.application.domain.post.ImageUrl
+import com.CbnuSwNotification.CbnuSwNotification.application.domain.post.Post
+import com.CbnuSwNotification.CbnuSwNotification.application.domain.post.QImageUrl.imageUrl
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.stereotype.Repository
+
+@Repository
+class DbImageUrlRepository(
+    private val em: EntityManager
+): ImageUrlRepository {
+
+    private val querydsl: JPAQueryFactory = JPAQueryFactory(em)
+    override fun save(imageUrl: ImageUrl): Long {
+        em.persist(imageUrl)
+        return imageUrl.id!!
+    }
+
+    override fun findById(id: Long): ImageUrl {
+        return em.find(ImageUrl::class.java, id)
+    }
+
+    override fun findAllByPost(post: Post): List<ImageUrl> {
+        return querydsl
+            .select(imageUrl)
+            .from(imageUrl)
+            .where(imageUrl.post.eq(post))
+            .fetch()
+    }
+}

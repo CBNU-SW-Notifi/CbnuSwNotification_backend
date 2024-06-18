8a61712fe817f848944d72129e12e25dd0c4791a
LEE026 <lhs456852@gmail.com>
2024-06-18 11:30:28 +0900

    CbnuSoftwareCrawlingLastIndex 주석 수정

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/crawling/lastIndex/domain/CbnuSoftwareCrawlingLastIndex.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/crawling/lastIndex/domain/CbnuSoftwareCrawlingLastIndex.kt
@@ -7,13 +7,22 @@ import jakarta.persistence.Id
 import jakarta.persistence.Lob
 
 /**
- * 충북대 소프트웨어 학과 크롤링 로그
+ * 해당 url의 마지막 크롤링 대상을 저장하기 위한 도메인
  */
 @Entity
 class CbnuSoftwareCrawlingLastIndex(
+    /**
+     * 크롤링 대상 페이지 주소
+     */
     @Lob
     val url: String,
-    val lastNum: Long,
+    @Lob
+    /**
+     * 해당 페이지에서 크롤링한 마지막 정보에 대한 내용
+     * 어떤 내용이든 들어갈 수 있도록 string으로 제작
+     * 크롤링시 추가적인 처리를 하여 사용
+     */
+    val lastIndex: String,
 ) {
     @Id
     @GeneratedValue

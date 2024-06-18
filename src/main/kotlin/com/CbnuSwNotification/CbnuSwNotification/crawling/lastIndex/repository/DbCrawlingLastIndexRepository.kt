268996e17999b189be1b5552cb61631acc53190e
LEE026 <lhs456852@gmail.com>
2024-06-18 15:44:06 +0900

    querydsl 관련 버그 수정

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/crawling/lastIndex/repository/DbCrawlingLastIndexRepository.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/crawling/lastIndex/repository/DbCrawlingLastIndexRepository.kt
@@ -1,7 +1,7 @@
 package com.CbnuSwNotification.CbnuSwNotification.crawling.lastIndex.repository
 
 import com.CbnuSwNotification.CbnuSwNotification.crawling.lastIndex.domain.CrawlingLastIndex
-import com.CbnuSwNotification.CbnuSwNotification.crawling.lastIndex.domain.QCbnuSoftwareCrawlingLastIndex.cbnuSoftwareCrawlingLastIndex
+import com.CbnuSwNotification.CbnuSwNotification.crawling.lastIndex.domain.QCrawlingLastIndex.crawlingLastIndex
 import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Repository
@@ -19,9 +19,9 @@ class DbCrawlingLastIndexRepository(
 
     override fun findByUrl(url: String): CrawlingLastIndex? {
         return querydsl
-            .select(cbnuSoftwareCrawlingLastIndex)
-            .from(cbnuSoftwareCrawlingLastIndex)
-            .where(cbnuSoftwareCrawlingLastIndex.url.eq(url))
+            .select(crawlingLastIndex)
+            .from(crawlingLastIndex)
+            .where(crawlingLastIndex.url.eq(url))
             .fetchOne()
     }
 

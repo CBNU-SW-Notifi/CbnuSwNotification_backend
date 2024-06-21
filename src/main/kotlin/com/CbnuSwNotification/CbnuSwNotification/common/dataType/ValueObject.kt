e343d89a0693485b6abcbb9102222cee507a770f
LEE026 <lhs456852@gmail.com>
2024-06-21 15:47:33 +0900

    checkValid로 명칭 수정

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/common/dataType/ValueObject.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/common/dataType/ValueObject.kt
@@ -13,6 +13,6 @@ abstract class ValueObject<T> {
      * 값이 조건에 맞는지 판단, 조건에 맞지 않을 경우 에러 발생
      * @param value 값이 조건에 맞는지 판단할 값
      */
-    protected abstract fun isValid(value: T)
+    protected abstract fun checkValid(value: T)
 
 }

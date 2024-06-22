a26d155cd26fb1d26ae022e19865eb106d4e78a3
LEE026 <lhs456852@gmail.com>
2024-06-22 18:19:54 +0900

    알림서비스 제작

---
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/notification/service/NotificationService.kt
@@ -0,0 +1,22 @@
+package com.CbnuSwNotification.CbnuSwNotification.notification.service
+
+import com.CbnuSwNotification.CbnuSwNotification.notification.android.AndroidNotification
+import com.CbnuSwNotification.CbnuSwNotification.notification.token.repository.TokenRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class NotificationService(
+    private val androidNotification: AndroidNotification,
+    private val tokenRepository: TokenRepository,
+) {
+    fun sendMessageToAllUser(title:String,body:String){
+        val tokens = tokenRepository.getAllDeviceToken()
+        androidNotification.sendMessage(tokens, title, body)
+    }
+
+    fun updateToken(id:String,token:String){
+        tokenRepository.updateTokenByAndroidId(id, token)
+    }
+}

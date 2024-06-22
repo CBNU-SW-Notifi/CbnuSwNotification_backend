0558c65f62c7528eaca8b5e9c6b7abf6b13ffbce
LEE026 <lhs456852@gmail.com>
2024-06-22 21:50:40 +0900

    컨트롤러에서 ok 응답 반환하게 수정

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/notification/controller/NotificationController.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/notification/controller/NotificationController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Notification API", description = "Notification  API 목록")
@@ -20,21 +21,25 @@ class NotificationController(
 
     @Operation(summary = "전체에게 메시지 보내기", description = "토큰이 저장되어 있는 모든 사용자에게 알림을 보냅니다.")
     @PostMapping("/sendToAll")
-    fun sendNotificationToAll(@RequestBody @Valid notificationRequest: NotificationRequest){
+    fun sendNotificationToAll(@RequestBody @Valid notificationRequest: NotificationRequest)
+            : ResponseEntity<Void> {
         log.info("전체에게 알림 발송 요청")
         notificationService.sendMessageToAllUser(
             title = notificationRequest.title,
             body = notificationRequest.body,
         )
+         return ResponseEntity.ok().build()
     }
 
     @Operation(summary = "토큰 저장", description = "토큰을 저장, 이미 토큰을 가진 id일 경우 덮어씀")
     @PutMapping("/saveToken")
-    fun saveToken(@RequestBody @Valid tokenUpdateRequest: TokenUpdateRequest){
+    fun saveToken(@RequestBody @Valid tokenUpdateRequest: TokenUpdateRequest)
+            : ResponseEntity<Void>{
         log.info("${tokenUpdateRequest.id}의 토큰 저장")
         notificationService.updateToken(
             id = tokenUpdateRequest.id,
             token = tokenUpdateRequest.token,
         )
+        return ResponseEntity.ok().build()
     }
 }

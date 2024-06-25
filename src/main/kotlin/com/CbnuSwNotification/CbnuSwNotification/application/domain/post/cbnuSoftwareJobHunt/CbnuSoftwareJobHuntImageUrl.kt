2cf76a28bc5e3966a12bd187f9f6108198ba4394
LEE026 <lhs456852@gmail.com>
2024-06-25 15:30:15 +0900

    imageUrl 구조 변경

--- src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/domain/post/cbnuSoftwareJobHunt/CbnuSoftwareJobHuntImageUrl.kt
+++ src/main/kotlin/com/CbnuSwNotification/CbnuSwNotification/application/domain/post/cbnuSoftwareJobHunt/CbnuSoftwareJobHuntImageUrl.kt
@@ -3,21 +3,16 @@ package com.CbnuSwNotification.CbnuSwNotification.application.domain.post.cbnuSo
 import jakarta.persistence.*
 
 @Entity
-class CbnuSoftwareJobHuntImageUrl {
+class CbnuSoftwareJobHuntImageUrl(
+    @Lob
+    val url: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="cbnu_software_job_hunt_post_id", nullable = false)
+    val post: CbnuSoftwareJobHuntPost,
+) {
     @Id
     @GeneratedValue
     @Column(name = "cbnu_software_job_hunt_image_url_id")
     val id: Long? = null
-
-    @Lob
-    val url: String
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="cbnu_software_job_hunt_post_id", nullable = false)
-    val post: CbnuSoftwareJobHuntPost
-
-    constructor(url: String, post: CbnuSoftwareJobHuntPost) {
-        this.url = url
-        this.post = post
-    }
 }
